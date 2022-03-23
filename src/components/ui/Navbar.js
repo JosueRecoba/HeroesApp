2ad4b9734c1ab24f3db166ee1abeba8ad70601a2
Josue <josue.huer.reco.22@gmial.com>
2022-03-23 00:22:06 -0600

    Navigate push/replace-useNavigate

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,10 +1,11 @@
-import { Link, NavLink } from 'react-router-dom'
+import React from 'react';
+import { Link, NavLink, useNavigate} from 'react-router-dom'
 
 export const Navbar = () => {
 
+    const navigate = useNavigate();
+
     const handleLogout = () => {
-        //TODO: Por hacer
-        console.log('logout')
     }
 
     return (
