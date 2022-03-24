56cb1ba8c5a0026def369be77e038965d8b60607
Josue <josue.huer.reco.22@gmial.com>
2022-03-23 22:17:43 -0600

    Aplicar filtro de Héroes - QueryString

--- src/selectors/getHeroesByName.js
+++ src/selectors/getHeroesByName.js
@@ -2,9 +2,11 @@ import { heroes } from '../data/heroes';
 
 export const getHeroesByName = (name = '') => {
 
-    //name = name.toLowerCase();
-    //return heroes.filter(hero => hero.name.toLowerCase().includes(name));
-
-    return [...heroes];
+    if ( name.length === 0 ) {
+        return [];
+    }
+    
+    name = name.toLowerCase();
+    return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
 
 } 
